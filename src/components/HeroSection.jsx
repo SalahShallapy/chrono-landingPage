c0c4f55aaafd798a91f763ed2d8954ce824b10b2
shalapy <salahshalapy999@gmail.com>
2025-06-16 22:03:15 +0300

    added documentation button and altered styes

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -12,12 +12,19 @@ const HeroSection = () => {
               sessions, and reminders - all in one simple Chrome extension.
             </p>
             <div className="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex">
-              <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-1 sm:gap-5">
+              <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
                 <a
                   href="#install"
-                  className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-500 hover:bg-indigo-600 sm:px-8"
+                  className="flex items-center justify-center px-4 py-3 border border-indigo-300 text-base font-medium rounded-md text-indigo-100 bg-transparent hover:bg-indigo-600 hover:border-indigo-600 transition-colors sm:px-8"
                 >
                   Get Started - It's Free
+                </a>
+                <a
+                  href="https://drive.google.com/file/d/1GZkF5rpttegE8MkWyCr84-AJ0AyA0JzQ/view?usp=sharing"
+                  target="blank"
+                  className="flex items-center justify-center px-4 py-3 border border-indigo-300 text-base font-medium rounded-md text-indigo-100 bg-transparent hover:bg-indigo-600 hover:border-indigo-600 transition-colors sm:px-8"
+                >
+                  Read Documentation
                 </a>
               </div>
             </div>
